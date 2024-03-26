58bad17223f02b1211e6a1dbf42d63fb35ab6aee
sosnovskiima <mikhailsosnovskii2805@gmail.com>
2024-03-26 13:23:33 +0400

    17

--- coursesgu/interface/Минипроект/Часть 2/part2.py
+++ coursesgu/interface/Минипроект/Часть 2/part2.py
@@ -4,19 +4,65 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QMainWindow
 
 
+def get_question():
+    with open('question.txt') as f:
+        temp = f.read()
+        if temp:
+            return temp
+        return 'Пока нет вопроса'
+
+
+def get_positive_voices():
+    with open('positive.txt') as f:
+        temp = f.read().split('\n')
+        if len(temp) > 1:
+            return temp[1:]
+        return list()
+
+
+def get_negative_voices():
+    with open('negative.txt') as f:
+        temp = f.read().split('\n')
+        if len(temp) > 1:
+            return temp[1:]
+        return list()
+
+
+def add_question_to_file(question):
+    with open('question.txt', 'w') as f:
+        f.write(question)
+
+
+def add_positive_voice_to_file(voice):
+    with open('positive.txt', 'a') as f:
+        f.write('\n')
+        f.write(voice)
+
+
+def add_negative_voice_to_file(voice):
+    with open('negative.txt', 'a') as f:
+        f.write('\n')
+        f.write(voice)
+
+
+def clear_voices():
+    with open('question.txt', 'w') as f:
+        f.write('')
+    with open('positive.txt', 'w') as f:
+        f.write('')
+    with open('negative.txt', 'w') as f:
+        f.write('')
+
+
 class DecisionAssistant(QMainWindow):
-    question = 'Пока нет вопроса'
-    positive_voices = list()
-    negative_voices = list()
+    question = get_question()
+    positive_voices = get_positive_voices()
+    negative_voices = get_negative_voices()
 
     def __init__(self):
         super().__init__()
         uic.loadUi('part2.ui', self)
-        self.get_voices()
-        self.label_question.setText(self.question)
-        self.list_positive.addItems(self.positive_voices)
-        self.list_negative.addItems(self.negative_voices)
-
+        self.update_after_restart()
         self.button_ask.clicked.connect(self.on_click_ask)
         self.button_positive.clicked.connect(self.on_click_positive_or_negative)
         self.button_negative.clicked.connect(self.on_click_positive_or_negative)
@@ -25,32 +71,55 @@ class DecisionAssistant(QMainWindow):
     def on_click_ask(self):
         if self.edit_question.text():
             self.question = self.edit_question.text()
-            self.label_question.setText(self.question)
-            self.edit_argument.setEnabled(True)
-            self.button_positive.setEnabled(True)
-            self.button_negative.setEnabled(True)
-            self.edit_question.clear()
-            self.edit_question.setEnabled(False)
-            self.button_ask.setEnabled(False)
+            add_question_to_file(self.question)
+            self.update_after_question()
 
     def on_click_positive_or_negative(self):
         if self.sender() is self.button_positive:
             argument = self.edit_argument.text()
-            self.list_positive.addItem(argument)
             self.positive_voices.append(argument)
-            self.add_positive()
-            self.label_positive.setText(f'Голосов: {len(self.positive_voices)}')
-            self.edit_argument.clear()
+            add_positive_voice_to_file(argument)
+            self.update_after_positive_voice(argument)
         else:
             argument = self.edit_argument.text()
-            self.list_negative.addItem(argument)
             self.negative_voices.append(argument)
-            self.add_negative()
-            self.label_negative.setText(f'Голосов: {len(self.negative_voices)}')
-            self.edit_argument.clear()
+            add_negative_voice_to_file(argument)
+            self.update_after_negative_voice(argument)
+        self.edit_argument.clear()
 
     def on_click_reset(self):
         self.question = 'Пока нет вопроса'
+        self.positive_voices.clear()
+        self.negative_voices.clear()
+        clear_voices()
+        self.update_after_reset()
+
+    def update_after_restart(self):
+        if self.question != 'Пока нет вопроса':
+            self.update_after_question()
+            self.list_positive.addItems(self.positive_voices)
+            self.list_negative.addItems(self.negative_voices)
+            self.label_positive.setText(f'Голосов: {len(self.positive_voices)}')
+            self.label_negative.setText(f'Голосов: {len(self.negative_voices)}')
+
+    def update_after_question(self):
+        self.label_question.setText(self.question)
+        self.edit_argument.setEnabled(True)
+        self.button_positive.setEnabled(True)
+        self.button_negative.setEnabled(True)
+        self.edit_question.clear()
+        self.edit_question.setEnabled(False)
+        self.button_ask.setEnabled(False)
+
+    def update_after_positive_voice(self, argument):
+        self.list_positive.addItem(argument)
+        self.label_positive.setText(f'Голосов: {len(self.positive_voices)}')
+
+    def update_after_negative_voice(self, argument):
+        self.list_negative.addItem(argument)
+        self.label_negative.setText(f'Голосов: {len(self.negative_voices)}')
+
+    def update_after_reset(self):
         self.label_question.setText(self.question)
         self.edit_argument.clear()
         self.edit_argument.setEnabled(False)
@@ -60,37 +129,8 @@ class DecisionAssistant(QMainWindow):
         self.label_negative.setText('Голосов: нет')
         self.list_positive.clear()
         self.list_negative.clear()
-        self.positive_voices.clear()
-        self.negative_voices.clear()
-        self.reset_voices()
         self.edit_question.setEnabled(True)
         self.button_ask.setEnabled(True)
-
-    def get_voices(self):
-        with open('question.txt') as f:
-            temp = f.read()
-            if temp:
-                self.question = temp
-        with open('positive.txt') as f:
-            self.positive_voices = f.read().split('\n')
-        with open('negative.txt') as f:
-            self.negative_voices = f.read().split('\n')
-
-    def add_positive(self):
-        with open('positive.txt', 'w') as f:
-            f.writelines(self.positive_voices)
-
-    def add_negative(self):
-        with open('negative.txt', 'w') as f:
-            f.writelines(self.negative_voices)
-
-    def reset_voices(self):
-        with open('question.txt', 'w') as f:
-            f.write('')
-        with open('positive.txt', 'w') as f:
-            f.write('')
-        with open('negative.txt', 'w') as f:
-            f.write('')
 
 
 if __name__ == '__main__':
