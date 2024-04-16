7b6bfc17d731466eb057b90a5ac34bc1a3378384
sosnovskiima <mikhailsosnovskii2805@gmail.com>
2024-04-16 13:28:34 +0400

    26

--- coursesgu/interface/Задание 3. Планировщик задач/task3.py
+++ coursesgu/interface/Задание 3. Планировщик задач/task3.py
@@ -1,11 +1,10 @@
 import sys
-
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtCore import QDate, QTime
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import QTime, QDate
 
 
-class EventPlanner(QMainWindow):
+class EventPlanner(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('task3.ui', self)
@@ -15,18 +14,19 @@ class EventPlanner(QMainWindow):
         event_name: str = self.edit_event_name.text()
         event_time: QTime = self.edit_event_time.time()
         event_date: QDate = self.edit_event_date.selectedDate()
-        self.list_of_events.addItem(
-            f'{event_name}, '
-            f'{event_time.hour():0>2}:{event_time.minute():0>2}, '
-            f'{event_date.day():0>2}.{event_date.month():0>2}.{event_date.year():0>2}'
-        )
-        self.update_after_event_add()
+        if event_name:
+            self.list_of_events.addItem(
+                f'{event_date.day():0>2}.{event_date.month():0>2}.{event_date.year():0>2}    '
+                f'{event_time.hour():0>2}:{event_time.minute():0>2}    '
+                f'{event_name}'
+            )
+            self.update_after_event_add()
 
     def update_after_event_add(self):
+        self.list_of_events.sortItems()
         self.edit_event_name.clear()
         self.edit_event_time.setTime(QTime(0, 0, 0, 0))
-        # self.calendarEvent.setSelectedDate(QDate.currentDate())
-        # Сортировка событий по времени наступления от самого раннего к самому позднему
+        self.edit_event_date.setSelectedDate(QDate.currentDate())
 
 
 if __name__ == '__main__':
