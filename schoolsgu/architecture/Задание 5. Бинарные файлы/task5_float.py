e5e81cdd196377c94a7a163f38b3c6f5d5837c52
sosnovskiima <mikhailsosnovskii@gmail.com>
2024-05-21 11:37:01 +0400

    42

--- schoolsgu/architecture/Задание 5. Бинарные файлы/task5_float.py
+++ schoolsgu/architecture/Задание 5. Бинарные файлы/task5_float.py
@@ -2,14 +2,13 @@ import struct
 
 result: bytes = bytes()
 while not result:
-    s = input('Введите вещественное число от -65504.0 до 65504.0: ')
-    if '.' not in str(s):
+    s = input('Введите вещественное число от 2.2250738585072014e-308 до 1.7976931348623157e+308: ')
+    try:
+        result: bytes = struct.pack('f', float(s))
+        print(f'Созданный бинарное файл из числа {s}: {str(result)[2:-1]}')
+    except ValueError:
         print(f'Строка {s} не является вещественным числом.')
-    else:
-        try:
-            result: bytes = struct.pack('e', float(s))
-            print(f'Созданный бинарное файл из числа {s}: {str(result)[2:-1]}')
-        except OverflowError:
-            print(f'Число {s} не соответствует двухбайтовому формату float.')
-number: float = struct.unpack('e', result)[0]
+    except OverflowError:
+        print(f'Число {s} не соответствует двухбайтовому формату float.')
+number: float = struct.unpack('f', result)[0]
 print(f'Беззнаковое число, полученное из бинарного файла: {number}')
--- schoolsgu/programming/10. Сортировки/Контест 10.1 Сортировки/7.py
+++ schoolsgu/programming/10. Сортировки/Контест 10.1 Сортировки/7.py
@@ -2,9 +2,15 @@ with open('input.txt', 'r') as file_in:
     n = int(file_in.readline())
     a = list()
     product, company, quantity = 'product', 'company', 'quantity'
-    for _ in range(n):
+    for i in range(n):
         p, c, q = file_in.readline().split()
         a.append({product: p, company: c, quantity: q})
+        j = i
+        while j > 0 and (p < a[j - 1][product] or (
+            p == a[j - 1][product] and int(q) > int(a[j - 1][quantity])
+        )):
+            a[j], a[j - 1] = a[j - 1], a[j]
+            j -= 1
     e = file_in.readline()
 with open('output.txt', 'w') as file_out:
     i = 0
