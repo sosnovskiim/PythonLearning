ca1ca5e6e9df23a2a9bdd7598338d16139d2634f
sosnovskiim <darkisosn2805@gmail.com>
2025-06-24 22:58:21 +0400

    48

--- codeforces/Алгоритмы на строках/H - Уотто и механизм.py
+++ codeforces/Алгоритмы на строках/H - Уотто и механизм.py
@@ -1,48 +1,35 @@
-def hash_abc_string(s: str, a: dict[str, int], b: int, p: int) -> int:
+def hash_abc(s: str, a: dict[str, int], b: list[int], p: int) -> int:
     h = 0
     for i in range(len(s)):
-        h += a[s[i]] * b ** (len(s) - i - 1)
-    return h % p
-
-
-def hash_abc_char(c: str, pow: int, a: dict[str, int], b: int, p: int) -> int:
-    return (a[c] * b ** pow) % p
-
-
-def is_hash_exists(a: list[int], h: int) -> bool:
-    left, right = 0, len(a) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if a[mid] == h:
-            return True
-        elif a[mid] < h:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return False
+        h += (a[s[i]] * b[i]) % p
+    return h
 
 
 n, m = map(int, input().split())
 a = {"a": ord("a"), "b": ord("b"), "c": ord("c")}
-b = 3
+b = [1] * 10 ** 6
 p = 10 ** 9 + 7
-memory = {hash_abc_string(input(), a, b, p) for _ in range(n)}
-# memory.sort()
+for i in range(1, len(b)):
+    b[i] = (5 * b[i - 1]) % p
+t = {hash_abc(input(), a, b, p) for _ in range(n)}
 r = ""
 for _ in range(m):
     s = input()
-    h = hash_abc_string(s, a, b, p)
+    # h = hash_abc(s, a, b, p)
     f = False
     for i in range(len(s)):
-        pow = len(s) - i - 1
-        h_old = hash_abc_char(s[i], pow, a, b, p)
-        for c in ("a", "b", "c"):
+        # h_old = (a[s[i]] * b[i]) % p
+        # h -= h_old
+        for c in a.keys():
             if c != s[i]:
-                h_new = hash_abc_char(c, pow, a, b, p)
-                # f = is_hash_exists(memory, h - h_old + h_new)
-                f = (h - h_old + h_new) in memory
+                h = hash_abc(s[:i] + c + s[i + 1:], a, b, p)
+                # h_new = (a[c] * b[i]) % p
+                # h += h_new
+                f = h in t
+                # h -= h_new
                 if f:
                     break
+        # h += h_old
         if f:
             r += "YES\n"
             break
