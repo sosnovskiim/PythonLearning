d577b7386760e2cd7d515bf61457a576d1af2150
sosnovskiim <sosnovskiim@ya.ru>
2025-06-29 00:42:16 +0400

    53

--- codeforces/Алгоритмы на строках/F - Марсианские строки.py
+++ codeforces/Алгоритмы на строках/F - Марсианские строки.py
@@ -1,45 +1,40 @@
-def z_func(s):
-    z = [0] * len(s)
+def get_pref_suff(len_p, p, s):
+    def get_char(i):
+        return p[i] if i < len_p + 1 else s[i - len_p - 1]
+
+    n = len_p + len_s + 1
+    z = [0] * n
+    dp = [inf] * (len_p + 2)
     left, right = 0, 0
-    for i in range(1, len(s)):
+    for i in range(1, n):
         if i <= right:
             z[i] = min(z[i - left], right - i + 1)
-        while i + z[i] < len(s) and s[z[i]] == s[i + z[i]]:
+        while i + z[i] < n and get_char(z[i]) == get_char(i + z[i]):
             z[i] += 1
         if z[i] > 0 and i + z[i] - 1 > right:
             left, right = i, i + z[i] - 1
-    return z
+        if i > len_p and z[i] > 0:
+            len_z = min(z[i], len_p)
+            if i < dp[len_z]:
+                dp[len_z] = i
+    for i in range(len_p, 0, -1):
+        if dp[i] > dp[i + 1]:
+            dp[i] = dp[i + 1]
+    return dp
 
 
 s = input()
-m = int(input())
-words = [input() for _ in range(m)]
+len_s = len(s)
+rev_s = s[::-1]
 inf = 10 ** 9
 cnt = 0
-for p in words:
+for _ in range(int(input())):
+    p = input()
     len_p = len(p)
     if len_p < 2:
         continue
-    t1 = p + "#" + s
-    z1 = z_func(t1)
-    pref = [inf] * (len_p + 2)
-    for i in range(len_p + 1, len(t1)):
-        len_z = z1[i]
-        if len_z != 0 and i < pref[len_z]:
-            pref[len_z] = i
-    for i in range(len_p, 0, -1):
-        if pref[i] > pref[i + 1]:
-            pref[i] = pref[i + 1]
-    t2 = p[::-1] + "#" + s[::-1]
-    z2 = z_func(t2)
-    suff = [inf] * (len_p + 2)
-    for i in range(len_p + 1, len(t2)):
-        len_z = z2[i]
-        if len_z != 0 and i < suff[len_z]:
-            suff[len_z] = i
-    for i in range(len_p, 0, -1):
-        if suff[i] > suff[i + 1]:
-            suff[i] = suff[i + 1]
+    pref = get_pref_suff(len_p, p + "#", s)
+    suff = get_pref_suff(len_p, p[::-1] + "#", rev_s)
     f = False
     for i in range(1, len_p):
         if pref[i] == inf or suff[len_p - i] == inf:
